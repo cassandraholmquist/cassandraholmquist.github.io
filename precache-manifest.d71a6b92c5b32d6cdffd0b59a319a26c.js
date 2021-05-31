f19cfb15597d3c7e604945a1e606294b719e9e9d
Cassandra Holmquist <71176486+cassandraholmquist@users.noreply.github.com>
2021-05-31 10:57:27 +0200

    first commit

---
+++ precache-manifest.d71a6b92c5b32d6cdffd0b59a319a26c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9381cd63c35871cb2e2",
+    "url": "/css/app.41b7c156.css"
+  },
+  {
+    "revision": "1fd1acf314db35afa4d8",
+    "url": "/css/chunk-vendors.55204a1e.css"
+  },
+  {
+    "revision": "038b235cc4ce46f5fc80",
+    "url": "/css/menu.41e47cf2.css"
+  },
+  {
+    "revision": "ea0a202169ef39589d14a397b4b7dfd6",
+    "url": "/img/icon@192px.ea0a2021.png"
+  },
+  {
+    "revision": "66b572c6ede18fb008c075a2989819b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9381cd63c35871cb2e2",
+    "url": "/js/app.c52dca80.js"
+  },
+  {
+    "revision": "1fd1acf314db35afa4d8",
+    "url": "/js/chunk-vendors.63edec48.js"
+  },
+  {
+    "revision": "038b235cc4ce46f5fc80",
+    "url": "/js/menu.696c80f1.js"
+  },
+  {
+    "revision": "e271d78d00a29f6f1ef8aafbb9db0566",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
